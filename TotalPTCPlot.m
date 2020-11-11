d8f25e2a8158a6c2fd649bbeb95791e82c003ac2
TotallySeriousDeveloper123 <38567675+TotallySeriousDeveloper123@users.noreply.github.com>
2020-11-11 13:45:41 +0000

    Update TotalPTCPlot.m

--- TotalPTCPlot.m
+++ TotalPTCPlot.m
@@ -1,26 +1,23 @@
 h = 132;    v = 142; Np = h*v; data = ones(h,v);   datar = ones(h,v);
 Contentsh = ["1","10","25","50","75","100","175","200","250","300","400","500","600","700","900","1100","1300","1500"];
 Contents = ["1","5","10","25","50","100","200","350","450","550","1000","2000","2500"];
-Var = 1:length(Contents); j=150;
-for j = 1:length(Contents);
-    raw = load(append('C:\Users\alasd\OneDrive\Documents\!UniWork\Project\Extra\csv_images\csv_images\'+string(Contents(j))+'.csv'));
-    rawr = load(append('C:\Users\alasd\OneDrive\Documents\!UniWork\Project\Extra\csv_images\csv_images\'+string(Contents(j))+'r.csv'));
+Var = 1:length(Contentsh); mean = 1:length(Contentsh); j=150;
+for j = 1:length(Contentsh)
+    raw = load(append('C:\Users\alasd\OneDrive\Documents\!UniWork\Project\Extra\csv_images\csv_images\h'+string(Contentsh(j))+'.csv'));
+    rawr = load(append('C:\Users\alasd\OneDrive\Documents\!UniWork\Project\Extra\csv_images\csv_images\h'+string(Contentsh(j))+'r.csv'));
     for i = 1:h                                                     %load arrays to match pictures
         data(i,:) = raw(((i-1)*v)+1:(i*v));
-        i=i+1;
     end
     for i = 1:h
         datar(i,:) = rawr(((i-1)*v)+1:(i*v));
-        i=i+1;
     end
     truepic = data(3:h,:);                  truepicr = datar(3:h,:);  %ignore top 2 rows of pixels
-    truepic = truepic - (sum(sum(truepic))./Np);     truepicr = truepicr - (sum(sum(truepicr))./Np);    %calculate offset
-    Var(j) = 0.5*(sum(sum((truepic-truepicr).^2))/Np);                                                  %Variance between images
-    j=j+1;
+    Var(j) = 0.5*(sum(sum((truepic-truepicr).^2))/Np);                %Variance between images
+    mean(j) = sum(sum(truepic(:,1:134)))/(130*134);
 end
-sigma = sqrt(Var)                                                 %RMS standard deviation
-Y = log10(sigma)                                                  %X and Y values to plot on graph
-X = log10(str2double(Contents))
-scatter(X,Y)
+sigma = sqrt(Var);                                                 %RMS standard deviation
+Y = log(sigma);                                                    %X and Y values to plot on graph
+X = log(mean);
+scatter(X,Y);
 xlabel('log(Intensity)')
 ylabel('log(RMS noise)')
